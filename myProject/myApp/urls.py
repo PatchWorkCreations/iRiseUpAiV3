a67c6bcd6ef1b39f7c6f17c83d1e51e662c5edbc
Julia <juliavictorio16@Gmail.com>
2024-11-04 01:43:12 +0800

    updated landing:

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('personalized-plan/', views.personalized_plan, name='personalized_plan'),
+    path('', views.personalized_plan, name='personalized_plan'),
     path('set-selected-plan/', views.setSelectedPlanInSession, name='set_selected_plan'),
     path('process-payment/', views.process_payment, name='process_payment'),
     path('grant-service-access/', views.grant_service_access, name='grant_service_access'),
